3c6326c4ce00250e4dc930e5e1255fc92125113a
PriyanshuSingh44 <priyanshu3303@gmail.com>
2025-08-08 21:17:16 +0530

    added edit profile feature

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from urllib.parse import urlsplit
 import sqlalchemy as sa
 from datetime import datetime, timezone
 from app import app, db
-from app.form import LoginForm, RegistrationForm
+from app.form import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 
 # Home page
@@ -89,3 +89,19 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.now(timezone.utc)
         db.session.commit()
+
+# Edit profile 
+@app.route('/edit_profile', methods=['GET','POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
